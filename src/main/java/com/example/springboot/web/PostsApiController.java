1718992da0cbf24291b4950680773d5f39ed605a
hyeonee <allohey0426@gmail.com>
2020-04-05 21:44:16 +0900

    posts update api 추가

--- src/main/java/com/example/springboot/web/PostsApiController.java
+++ src/main/java/com/example/springboot/web/PostsApiController.java
@@ -1,11 +1,11 @@
 package com.example.springboot.web;
 
 import com.example.springboot.service.posts.PostsService;
+import com.example.springboot.web.dto.PostsResponseDto;
 import com.example.springboot.web.dto.PostsSaveRequestDto;
+import com.example.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
@@ -18,5 +18,14 @@ public class PostsApiController {
         return postsService.save(requestDto);
     }
 
-    
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        System.out.println("update~~~~~~~~~controller");
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
+    }
 }
