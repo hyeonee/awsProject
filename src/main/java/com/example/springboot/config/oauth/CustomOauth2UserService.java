ec2767b306b30c1bca50253a999358312e061b3c
hyeonee <allohey0426@gmail.com>
2020-04-19 23:34:25 +0900

    spring security 적용 (oauth2)

---
+++ src/main/java/com/example/springboot/config/oauth/CustomOauth2UserService.java
@@ -0,0 +1,55 @@
+package com.example.springboot.config.oauth;
+
+import com.example.springboot.config.oauth.dto.OAuthAttributes;
+import com.example.springboot.config.oauth.dto.SessionUser;
+import com.example.springboot.domain.user.User;
+import com.example.springboot.domain.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+import java.util.Collections;
+
+@RequiredArgsConstructor
+@Service
+public class CustomOauth2UserService implements OAuth2UserService<OAuth2UserRequest, OAuth2User> {
+
+    private final UserRepository userRepository;
+    private final HttpSession httpSession;
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+
+        OAuth2UserService delgate = new DefaultOAuth2UserService();
+        OAuth2User oAuth2User = delgate.loadUser(userRequest);
+
+        String regitrationId = userRequest.getClientRegistration().getRegistrationId();
+        String userNameAttributeName = userRequest.getClientRegistration().getProviderDetails()
+                .getUserInfoEndpoint().getUserNameAttributeName();
+
+        OAuthAttributes attributes = OAuthAttributes.of(regitrationId, userNameAttributeName, oAuth2User.getAttributes());
+        User user = saveOrUpdate(attributes);
+
+        httpSession.setAttribute("user", new SessionUser(user));
+
+
+        return new DefaultOAuth2User(Collections.singleton(new SimpleGrantedAuthority(user.getRoleKey()))
+                , attributes.getAttributes()
+                , attributes.getNameAttributeKey());
+    }
+
+    private User saveOrUpdate(OAuthAttributes attributes) {
+        User user = userRepository.findByEmail(attributes.getEmail())
+                .map(entity -> entity.update(attributes.getName(), attributes.getPicture()))
+                .orElse(attributes.toEntity());
+
+        return userRepository.save(user);
+    }
+}
---
+++ src/main/java/com/example/springboot/config/oauth/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.example.springboot.config.oauth;
+
+
+import com.example.springboot.domain.user.Role;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final CustomOauth2UserService customOauth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                .authorizeRequests().mvcMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
+                .mvcMatchers("/api/v1/**").hasRole(Role.USER.name())
+                .anyRequest().authenticated()
+                .and()
+                .logout()
+                .logoutSuccessUrl("/")
+                .and()
+                .oauth2Login()
+                .userInfoEndpoint()
+                .userService(customOauth2UserService)
+
+        ;
+    }
+
+}
